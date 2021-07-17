6c11e92cb4cadfa6e24f98ca25b4857f5e2e136d
godzhang <zhangqijilindaxue@126.com>
2021-07-17 14:44:22 +0800

    feat: 创建errorboundary

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+import { Component, PropsWithChildren, ReactElement, ReactNode } from "react";
+
+type FallbackRender = (props: { error: Error | null }) => ReactElement;
+type IErrorBoundaryProps = PropsWithChildren<{
+  fallbackRender: FallbackRender;
+}>;
+
+export class ErrorBoundary extends Component<
+  IErrorBoundaryProps,
+  { error: Error | null }
+> {
+  state = { error: null };
+
+  static getDerivedStateFromError(error: Error) {
+    return { error };
+  }
+
+  render() {
+    const { error } = this.state;
+    const { children, fallbackRender } = this.props;
+    if (error) {
+      return fallbackRender({ error });
+    }
+    return children;
+  }
+}
